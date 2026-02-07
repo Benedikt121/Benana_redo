4fc1dd24508ad853e94ec18d08d53b76de22375a
notBene <bene.boeckle@gmail.com>
2026-02-07 14:41:56 +0100

    Made server.ts serve the html file

--- benana_backend/src/server.ts
+++ benana_backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
@@ -7,6 +8,15 @@ app.get("/hello", (req, res) => {
 });
 
 const PORT = 5001;
+
+const clientPath = path.join(__dirname, "../client");
+app.use(express.static(clientPath));
+
+// 2. Catch-All Route: Alles was keine API ist, geht an React (index.html)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(clientPath, "index.html"));
+});
+
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
