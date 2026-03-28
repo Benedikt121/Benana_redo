4aed1677c05cd8c4e546bce8bb6109b59aea8a32
notBene <bene.boeckle@gmail.com>
2026-03-28 14:23:52 +0100

    Initiated expo frontend

---
+++ benana-frontend/src/utils/storage.ts
@@ -0,0 +1,20 @@
+import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
+
+const TOKEN_KEY = "benana_jwt_token";
+
+export const saveToken = async (token: string) => {
+  if (Platform.OS === "web") {
+    localStorage.setItem(TOKEN_KEY, token);
+  } else {
+    await SecureStore.setItemAsync(TOKEN_KEY, token);
+  }
+};
+
+export const getToken = async () => {
+  if (Platform.OS === "web") {
+    return localStorage.getItem(TOKEN_KEY);
+  } else {
+    return await SecureStore.getItemAsync(TOKEN_KEY);
+  }
+};
