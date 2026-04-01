caf6d9f0d31f418467012ff65e838bef37e09f82
notBene <bene.boeckle@gmail.com>
2026-04-01 15:25:50 +0200

    Background jetzt weiß :(

--- benana-frontend/src/app/index.tsx
+++ benana-frontend/src/app/index.tsx
@@ -2,9 +2,9 @@ import DeepWaterBackground from "@/components/background/deepWaterBackground";
 import { View } from "react-native";
 
 export default function Index() {
-  return (
-    <View style={{ flex: 1, backgroundColor: "black" }}>
-      
-    </View>
-  );
+  return <View className="flex-1">
+    <p>
+      Test
+    </p>
+  </View>;
 }
