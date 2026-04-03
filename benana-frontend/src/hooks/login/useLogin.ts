289d13ebd1dfe5c023b7b6546cdb4ebc3a67a4d4
notBene <bene.boeckle@gmail.com>
2026-04-03 15:08:39 +0200

    fixed bcrypt error

--- benana-frontend/src/hooks/login/useLogin.ts
+++ benana-frontend/src/hooks/login/useLogin.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { useAuthStore } from "@/store/auth.store";
 import { useRouter } from "expo-router";
 import { useState } from "react";
