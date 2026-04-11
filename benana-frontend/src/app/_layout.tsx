2ada105a0f08a15e5fcf5eb2ea740c8ddb700071
notBene <bene.boeckle@gmail.com>
2026-04-11 19:11:50 +0200

    i kon net mer komische bug jo

--- benana-frontend/src/app/_layout.tsx
+++ benana-frontend/src/app/_layout.tsx
@@ -2,24 +2,18 @@ import { Stack, useRouter, useSegments } from "expo-router";
 import "../global.css";
 import { ActivityIndicator, View } from "react-native";
 import DeepWaterBackground from "@/components/background/deepWaterBackground/deepWaterBackground";
-import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import RainyWindowBackground from "@/components/background/rainyWindowBackground/rainyWindowBackground";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAuthStore } from "@/store/auth.store";
 import { useMusicColors } from "@/utils/useMusicColors";
 import { useMusicSync } from "@/hooks/sockets/useMusicSync";
 import { useUserStore } from "@/store/user.store";
-import { useInitialData } from "@/hooks/login/useInitialData";
 import { useGlobalSocket } from "@/hooks/sockets/useGlobalSocket";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  const TransparentTheme = {
-    ...DefaultTheme,
-    background: "transparent",
-  };
   // should be switchable in the future and saved in a cookie/local storage
   const usedBackground = useUserStore(
     (state) => state.profile?.preferedBackground,
@@ -33,8 +27,6 @@ export default function RootLayout() {
     hydrate();
   }, []);
 
-  const { isLoading } = useInitialData();
-
   useEffect(() => {
     if (!isHydrated) return;
 
@@ -47,34 +39,22 @@ export default function RootLayout() {
     }
   }, [token, isHydrated, segments]);
 
-  if (!isHydrated || (token && isLoading)) {
-    return (
-      <View className="flex-1 justify-center items-center bg-black">
-        <ActivityIndicator size="large" color="#ffffff" />
-      </View>
-    );
-  }
-
-  useGlobalSocket();
   useMusicColors();
-  useMusicSync();
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider value={TransparentTheme}>
-        <View className="flex-1 bg-transparent">
-          {usedBackground === "deepWater" ? (
-            <DeepWaterBackground coverUrl="https://i.scdn.co/image/ab67616d0000b27346f6a37af54494f2b038eaf0" />
-          ) : (
-            <RainyWindowBackground coverUrl="https://i.scdn.co/image/ab67616d0000b27346f6a37af54494f2b038eaf0" />
-          )}
-          <Stack
-            screenOptions={{
-              headerShown: false,
-              contentStyle: { backgroundColor: "transparent" },
-            }}
-          />
-        </View>
-      </ThemeProvider>
+      <View className="flex-1 bg-transparent">
+        {usedBackground === "deepWater" ? (
+          <DeepWaterBackground coverUrl="https://i.scdn.co/image/ab67616d0000b27346f6a37af54494f2b038eaf0" />
+        ) : (
+          <RainyWindowBackground coverUrl="https://i.scdn.co/image/ab67616d0000b27346f6a37af54494f2b038eaf0" />
+        )}
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: "transparent" },
+          }}
+        />
+      </View>
     </QueryClientProvider>
   );
 }
--- benana-frontend/src/app/index.tsx
+++ benana-frontend/src/app/index.tsx
@@ -1,14 +1,21 @@
+import { useInitialData } from "@/hooks/login/useInitialData";
+import { useGlobalSocket } from "@/hooks/sockets/useGlobalSocket";
+import { useMusicSync } from "@/hooks/sockets/useMusicSync";
 import { useAuthStore } from "@/store/auth.store";
 import { useRouter } from "expo-router";
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, ActivityIndicator } from "react-native";
 
 export default function Index() {
+  const { isLoading } = useInitialData();
   const router = useRouter();
   const logout = useAuthStore((state) => state.logout);
   const handleLogout = () => {
     logout();
     router.replace("/login");
   };
+
+  useGlobalSocket();
+  useMusicSync();
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-white text-3xl font-bold mb-8 text-shadow-glow">
